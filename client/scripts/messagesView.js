b8e221b7b49096839f659d64190fdb23cbfd5ac0
anton10Luna <antonio.gilluna@gmail.com>
2020-07-13 16:13:31 -0700

    made ajax behavior tests pass

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -5,7 +5,15 @@ var MessagesView = {
   initialize: function() {
   },
 
-  render: function() {
+  render: function(message) {
+    // takes in a message as parameter
+
+    // should add messeges to the DOM
+    // ? looks into masseges for a massege
+    // should append to $chats (a div full of messages ?)
+
+
+
   }
 
 };
